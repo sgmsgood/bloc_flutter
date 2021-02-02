989a272b743c6c8c6c310305428da99cb4931383
sgmsgood <sgms_good@naver.com>
2021-02-03 07:37:09 +0900

    1. 2개의 builder로 감싸져 있을 때, 각각 다른 state 받는 방법

--- bloc_flutter_test/lib/main.dart
+++ bloc_flutter_test/lib/main.dart
@@ -1,38 +1,17 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onEvent(Bloc bloc, Object event) {
-    print(event);
-    super.onEvent(bloc, event);
-  }
-
-  @override
-  void onChange(Cubit cubit, Change change) {
-    print(change);
-    super.onChange(cubit, change);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
-    super.onTransition(bloc, transition);
-  }
-
-  @override
-  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print(error);
-    super.onError(cubit, error, stackTrace);
-  }
-}
-
 void main() {
-  Bloc.observer = SimpleBlocObserver();
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  var _counterCubit1 = CounterCubit();
+  var _counterCubit2 = CounterCubit();
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -40,103 +19,108 @@ class App extends StatelessWidget {
       child: BlocBuilder<ThemeCubit, ThemeData>(
         builder: (_, theme) {
           return MaterialApp(
-            theme: theme,
-            home: BlocProvider(
-              create: (_) => CounterBloc(),
-              child: CounterPage(),
-              )
-          );
+              theme: theme,
+              home: BlocProvider(
+                create: (_) => CounterCubit(),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Scaffold(
+                        appBar: AppBar(title: const Text('Counter')),
+                        body: BlocBuilder<CounterCubit, int>(
+                          cubit: _counterCubit1,
+                          builder: (_, count) {
+                            log("@!!count: $count");
+                            return Column(
+                              children: [
+                                Center(
+                                  child: Text('$count',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline1),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        floatingActionButton: CounterPage(),
+                      ),
+                    ),
+                    Expanded(
+                      child: Scaffold(
+                        appBar: AppBar(title: const Text('Counter')),
+                        body: BlocBuilder<CounterCubit, int>(
+                          cubit: _counterCubit2,
+                          builder: (_, count) {
+                            log("@!!count: $count");
+                            return Column(
+                              children: [
+                                Center(
+                                  child: Text('$count',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline1),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        floatingActionButton: CounterPage(),
+                      ),
+                    ),
+                  ],
+                ),
+              ));
         },
-        cubit: ThemeCubit(),
       ),
     );
   }
 }
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Counter')),
-      body: BlocBuilder<CounterBloc, int>(
-        builder: (_, count) {
-          return Column(
-            children: [
-              Center(
-                child: Text('$count',
-                    style: Theme.of(context).textTheme.headline1),
-              ),
-
-            ],
-          );
-        },
-      ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () =>
-                  context.read<CounterBloc>().add(CounterEvent.increment),
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              context.read<CounterCubit>().increment();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.remove),
-              onPressed: () =>
-                  context.read<CounterBloc>().add(CounterEvent.decrement),
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () {
+              context.read<CounterCubit>().decrement();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.brightness_6),
-              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: FloatingActionButton(
+            child: const Icon(Icons.brightness_6),
+            onPressed: () => context.read<ThemeCubit>().toggleTheme(),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              backgroundColor: Colors.red,
-              child: const Icon(Icons.error),
-              onPressed: () => context.read<CounterBloc>().add(null),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
 
-enum CounterEvent {
-  /// Notifies bloc to increment state.
-  increment,
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
 
-  /// Notifies bloc to decrement state.
-  decrement
-}
+  void increment() => {log("@!!state: $state"), emit(state + 1)};
 
-class CounterBloc extends Bloc<CounterEvent, int> {
-  /// {@macro counter_bloc}
-  CounterBloc() : super(0);
-
-  @override
-  Stream<int> mapEventToState(CounterEvent event) async* {
-    switch (event) {
-      case CounterEvent.decrement:
-        yield state - 1;
-        break;
-      case CounterEvent.increment:
-        yield state + 1;
-        break;
-      default:
-        addError(Exception('unsupported event'));
-    }
-  }
+  void decrement() => {log("@!!state: $state"), emit(state - 1)};
 }
 
 class ThemeCubit extends Cubit<ThemeData> {
